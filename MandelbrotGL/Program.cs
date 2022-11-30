f5904b2e75ed6842b3bebf656d646faf898dc07b
Leandro Brás <shadowgaming.7@gmail.com>
2022-11-30 06:18:00 +0000

    Draw a rectange to the screen

--- MandelbrotGL/Program.cs
+++ MandelbrotGL/Program.cs
@@ -1,11 +1,14 @@
-﻿namespace MandelbrotGL
+﻿using OpenTK.Windowing.Desktop;
+
+namespace MandelbrotGL
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             // Creates a new OpenGL window and runs it
-            new Window(800, 600, "Mandlebrot").Run();
+            Window window = new Window();
+            window.Run();
         }
     }
 }
